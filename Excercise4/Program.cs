0ded5a23d2497b47b4fba9f51610ad0759711365
M S I <M S I@MSI>
2022-12-05 15:38:16 +0700

    Add project files.

---
+++ Excercise4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Excercise4
+{
+    class pop
+    {
+        public int info;
+        public pop next;
+        public pop(int i, pop n)
+        {
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
