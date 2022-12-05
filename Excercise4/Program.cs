f0764d4c3f00948f09bdecb759abf14d1e1412c1
M S I <M S I@MSI>
2022-12-05 16:03:42 +0700

    add console writline

--- Excercise4/Program.cs
+++ Excercise4/Program.cs
@@ -65,13 +65,20 @@ namespace Excercise4
 
             }
         }
-    }
 
-    
-    internal class Program
-    {
         static void Main(string[] args)
         {
+            Stack s = new Stack();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("\n*****Stack Menu*****");
+                Console.WriteLine("1. pop");
+                Console.WriteLine("2. push");
+                Console.WriteLine("3. Display");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine("\nEnter your Choice: ");
+            }
         }
     }
 }
